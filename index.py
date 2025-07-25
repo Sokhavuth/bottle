a120c862af19e9e118b58975e389aa15687222fe
khmerweb <vuthdevelop1@gmail.com>
2025-07-25 19:26:26 +0700

    Make it better

--- index.py
+++ index.py
@@ -4,12 +4,12 @@ from routes.admin import index as admin
 from routes import login
 from bottle import static_file, request, Bottle
 from config import settings
-from bottle_session import SessionPlugin
+#from bottle_session import SessionPlugin
 
-plugin = SessionPlugin(cookie_lifetime=600)
-index.app.install(plugin)
-login.app.install(plugin)
-admin.app.install(plugin)
+#plugin = SessionPlugin(cookie_lifetime=600)
+#index.app.install(plugin)
+#login.app.install(plugin)
+#admin.app.install(plugin)
 
 app = Bottle()
 
